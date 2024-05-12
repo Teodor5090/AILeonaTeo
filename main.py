50c9dd133fbd4ddbfcacbaffca28ac5bac477e01
Teodor5090 <132367425+Teodor5090@users.noreply.github.com>
2024-05-12 16:32:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from tensorflow.keras.models import load_model
 
-# Step 1: Load Image Data
+
 data_dir = './flowers'
 batch_size = 32
 image_size = (150, 150)
@@ -25,7 +25,7 @@ validation_generator = train_datagen.flow_from_directory(
     class_mode='categorical',
     subset='validation')
 
-# Step 4: Choose a Model
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
@@ -38,12 +38,12 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(5, activation='softmax')  # 5 output classes (assuming 5 flower species)
 ])
 
-# Step 5: Compile the Model
+
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-# Step 6: Train the Model
+
 history = model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // batch_size,
@@ -51,11 +51,11 @@ history = model.fit(
     validation_steps=validation_generator.samples // batch_size,
     epochs=10)
 
-# Step 7: Evaluate the Model
+
 test_loss, test_accuracy = model.evaluate(validation_generator, steps=validation_generator.samples // batch_size)
 print("Test Accuracy:", test_accuracy)
 
-# Step 8: Save the Model
+
 model.save("flower_classifier_model.h5")
 
 
